05e555497ab7ade895c5c29768b6dd7263c0d584
Maria Gorinova <m.gorinova@gmail.com>
2020-09-13 23:27:01 +0100

    Bugfix

--- SlicStan/Examples.fs
+++ SlicStan/Examples.fs
@@ -172,30 +172,30 @@ module ExamplesModule =
     data real y3 ~ normal(phi[z3], 1);
     "
 
-let discrete_ifs_sep = "
+    let discrete_ifs_sep = "
     
-int<2> z1 ~ bernoulli(0.5);
-int<2> z2;
-int<2> z3;
-data real[2] phi;
+    int<2> z1 ~ bernoulli(0.5);
+    int<2> z2;
+    int<2> z3;
+    data real[2] phi;
 
-if (z1 > 0) {
-    z2 ~ bernoulli(phi[z1]);
-    z3 ~ bernoulli(phi[z2]);
-}
-else {
-    z2 ~ bernoulli(phi[z1]);
-    z3 ~ bernoulli(phi[z1]);    
-}
+    if (z1 > 0) {
+        z2 ~ bernoulli(phi[z1]);
+        z3 ~ bernoulli(phi[z2]);
+    }
+    else {
+        z2 ~ bernoulli(phi[z1]);
+        z3 ~ bernoulli(phi[z1]);    
+    }
     
-data real y1 ~ normal(phi[z1], 1);
-data real y2 ~ normal(phi[z2], 1);
-data real y3 ~ normal(phi[z3], 1);    
-"
+    data real y1 ~ normal(phi[z1], 1);
+    data real y2 ~ normal(phi[z2], 1);
+    data real y3 ~ normal(phi[z3], 1);    
+    "
 
-let discrete_hmm_weird = "
-data real[2] phi;
-data real[2] theta;
+    let discrete_hmm_weird = "
+    data real[2] phi;
+    data real[2] theta;
 
     int<2> z1 ~ bernoulli(theta[1]);
     int<2> z2 ~ bernoulli(theta[z1]*theta[z1]);
